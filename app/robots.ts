ce14c78621ab0aa8024bada8581431a528b8f545
Raka Dwi Ananta <fahriwangsaf414@gmail.com>
2025-08-20 15:03:59 +0700

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -8,10 +8,95 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/api/', '/_next/', '/admin/']
+        disallow: [
+          '/api/',
+          '/_next/',
+          '/admin/',
+          '/*.json$',
+          '/*?*',
+          '/404',
+          '/500'
+        ]
+      },
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+        disallow: [
+          '/api/',
+          '/_next/',
+          '/admin/'
+        ]
+      },
+      {
+        userAgent: 'Googlebot-Image',
+        allow: '/',
+        disallow: [
+          '/api/',
+          '/_next/',
+          '/admin/'
+        ]
+      },
+      {
+        userAgent: 'Bingbot',
+        allow: '/',
+        disallow: [
+          '/api/',
+          '/_next/',
+          '/admin/'
+        ]
+      },
+      {
+        userAgent: 'Slurp',
+        allow: '/',
+        disallow: [
+          '/api/',
+          '/_next/',
+          '/admin/'
+        ]
+      },
+      {
+        userAgent: 'DuckDuckBot',
+        allow: '/',
+        disallow: [
+          '/api/',
+          '/_next/',
+          '/admin/'
+        ]
+      },
+      {
+        userAgent: 'facebookexternalhit',
+        allow: '/',
+        disallow: []
+      },
+      {
+        userAgent: 'Twitterbot',
+        allow: '/',
+        disallow: []
+      },
+      {
+        userAgent: 'LinkedInBot',
+        allow: '/',
+        disallow: []
+      },
+      {
+        userAgent: 'WhatsApp',
+        allow: '/',
+        disallow: []
+      },
+      {
+        userAgent: 'applebot',
+        allow: '/',
+        disallow: [
+          '/api/',
+          '/_next/',
+          '/admin/'
+        ]
       }
     ],
-    sitemap: `${BASE_URL}/sitemap.xml`,
-    host: BASE_URL.replace(/https?:\/\//, ''),
+    sitemap: [
+      `${BASE_URL}/sitemap.xml`,
+      `${BASE_URL}/sitemap-0.xml`
+    ],
+    host: BASE_URL,
   }
 }
